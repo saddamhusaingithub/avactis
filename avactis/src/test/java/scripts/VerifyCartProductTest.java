5c09c55b96747f9b49074e1691547f637fff0aa2
saddam Husain <saddamali13788@yahoo.com>
2022-06-14 23:48:56 +0530

    Added loggers

--- avactis/src/test/java/scripts/VerifyCartProductTest.java
+++ avactis/src/test/java/scripts/VerifyCartProductTest.java
@@ -13,6 +13,7 @@ import objectRepository.CheckoutPage;
 import objectRepository.OrderPlacedPage;
 import objectRepository.ProductListPage;
 import utility.ExcelUtils;
+import utility.Log;
 
 public class VerifyCartProductTest extends LoginTest {
 
@@ -66,7 +67,7 @@ public class VerifyCartProductTest extends LoginTest {
 				ExcelUtils.getCellValueByHeaderName("UserDetails", "Phone Number", 1) } };
 	}
 
-	@Test(dataProvider = "ProductDetails", priority = 1)
+	@Test(dataProvider = "ProductDetails")
 	public void verifyAddedProductsInUserCart(String productCategory1, String productSubCategory1, String product1,
 			String productCategory2, String product2, String productCategory3, String product3, int productQty1,
 			int productQty2, int productQty3) throws InterruptedException {
@@ -74,44 +75,62 @@ public class VerifyCartProductTest extends LoginTest {
 		// "9759064799")
 		// .clickOnSignInBtn(); //Comment this step for guest user
 
-		// Add First product and get its price
+		Log.startTestCase("verifyAddedProductsInUserCart");
+		
+		Log.info("Add First product and get its price");
 		product1Price = objAvactisHomePage.hoverOnProductCategories(productCategory1)
 				.clickOnProductSubCategories(productSubCategory1).addProductInAddToCart(product1, productQty1)
 				.getPriceOfAddedCartProducts(product1);
-		// Add second product and get its price
+
+		Log.info("Add second product and get its price");
 		product2Price = objAvactisHomePage.clickOnProductCategories(productCategory2)
 				.addProductInAddToCart(product2, productQty2).getPriceOfAddedCartProducts(product2);
-		// Add third product and get its price
+		
+		Log.info("Add third product and get its price");
 		objProductListPage = objAvactisHomePage.clickOnProductCategories(productCategory3)
 				.addProductInAddToCart(product3, productQty3);
 		product3Price = objProductListPage.getPriceOfAddedCartProducts(product3);
-		// Click on Cart button showing total count of added products
+		
+		Log.info("Click on Cart button showing total count of added products");
 		objProductListPage.clickOnTotalCartCounts();
 
-		// Verify added products in user cart
+		Log.info("Verify added products in user cart");
 		assertTrue(objProductListPage.verifyAddedProductInCart(product1), product1 + " fails to add in cart");
 		assertTrue(objProductListPage.verifyAddedProductInCart(product2), product2 + " fails to add in cart");
 		assertTrue(objProductListPage.verifyAddedProductInCart(product3), product3 + " fails to add in cart");
+		
+		Log.endTestCase("verifyAddedProductsInUserCart");
 	}
 
-	@Test(dataProvider = "UserCheckoutDetails", priority = 2)
+	@Test(dataProvider = "UserCheckoutDetails", dependsOnMethods = { "verifyAddedProductsInUserCart" })
 	public void VerifyCheckoutBillingAddress(String firstName, String lastName, String emailAddress, String country,
 			String zip, String state, String city, String addressLine1, String addressLine2, String phoneNumber) {
+		
+		Log.startTestCase("VerifyCheckoutBillingAddress");
 		objCheckoutPage = objProductListPage.clickOnCheckout();
 
+		Log.info("Enter Billing Address");
 		objCheckoutPage.enterFirstName(firstName).enterLastName(lastName).enterEmail(emailAddress)
 				.selectCountry(country).enterZipCode(zip).selectState(state).enterCity(city)
 				.enterAddressLine1(addressLine1).enterAddressLine2(addressLine2).enterPhoneNumber(phoneNumber)
 				.clickOnShippingCheckBox().ClickonContinueCheckoutBtn();
 
+		Log.info("Verify Billing Address after checkout");
 		assertTrue(objCheckoutPage.verifyBillingAddressAfterCheckout(addressLine1),
 				"Address is not saved or coming incorrect");
+		
+		Log.endTestCase("VerifyCheckoutBillingAddress");
 	}
 
-	@Test(priority = 3)
+	@Test(dependsOnMethods = { "VerifyCheckoutBillingAddress" })
 	public void verifyFinalPriceOfAddedProducts() {
+		
+		Log.startTestCase("verifyFinalPriceOfAddedProducts");
+		
+		Log.info("Choose shipping option as Ground and checkout");
 		objCheckoutPage.chooseShippingOptions(shippingOption).ClickonContinueCheckoutBtnOnBillingPage();
 
+		Log.info("Verify Final Products with Price and quantity");
 		assertEquals(productQty1, objCheckoutPage.getProductQuantity(product1));
 		assertEquals(productQty2, objCheckoutPage.getProductQuantity(product2));
 		assertEquals(productQty3, objCheckoutPage.getProductQuantity(product3));
@@ -119,21 +138,29 @@ public class VerifyCartProductTest extends LoginTest {
 		assertEquals(product2Price, objCheckoutPage.getProductCheckoutSalePrice(product2));
 		assertEquals(product3Price, objCheckoutPage.getProductCheckoutSalePrice(product3));
 
+		Log.endTestCase("verifyFinalPriceOfAddedProducts");
 	}
 
-	@Test(priority = 4)
+	@Test(dependsOnMethods = { "verifyFinalPriceOfAddedProducts" })
 	public void verifyFinalOrderPlaced() {
+		Log.startTestCase("verifyFinalOrderPlaced");
+		
+		Log.info("Place final order");
 		objOrderPlacedPage = objCheckoutPage.clickOnPlaceOrder();
 
+		Log.info("Verify Oder Id, Payment and Shipping method");
 		assertTrue(!objOrderPlacedPage.getOrderDetailsForGivenFields("Order Id").isEmpty());
 		assertEquals(objOrderPlacedPage.getOrderDetailsForGivenFields("Payment Method"), paymentMethod);
 		assertEquals(objOrderPlacedPage.getOrderDetailsForGivenFields("Shipping Method"), shippingOption);
 
+		Log.info("Verify Final Products with Price and quantity");
 		assertEquals(productQty1, objCheckoutPage.getProductQuantity(product1));
 		assertEquals(productQty2, objCheckoutPage.getProductQuantity(product2));
 		assertEquals(productQty3, objCheckoutPage.getProductQuantity(product3));
 		assertEquals(product1Price, objCheckoutPage.getProductCheckoutSalePrice(product1));
 		assertEquals(product2Price, objCheckoutPage.getProductCheckoutSalePrice(product2));
 		assertEquals(product3Price, objCheckoutPage.getProductCheckoutSalePrice(product3));
+		
+		Log.endTestCase("verifyFinalOrderPlaced");
 	}
 }
--- avactis/src/test/java/scripts/VerifyRegisterationTest.java
+++ avactis/src/test/java/scripts/VerifyRegisterationTest.java
@@ -7,6 +7,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import objectRepository.RegisterPage;
+import utility.Log;
 
 public class VerifyRegisterationTest extends LoginTest {
 	WebDriver driver;
@@ -15,23 +16,37 @@ public class VerifyRegisterationTest extends LoginTest {
 
 	@Test
 	public void verifyRegisterationAffirmative() {
+		Log.startTestCase("verifyRegisterationAffirmative");
+		
+		Log.info("Entering valid user details for successfull registeration");
 		objAvactisHomePage.clickOnMyAccount().clickOnRegisterButton().enterEmail("zia2208@gmail.com")
 				.enterPassword("9759064799").enterReTypePassword("9759064799").enterFirstName("Mumtaz")
 				.enterLastName("Husain").selectCountry("India").selectState("Gujarat").enterZipCode("380055")
 				.enterCity("Ahmedabad").enteraddressLine1(" c-101, Al arsh-3").enteraddressLine2("juhapura")
 				.enterPhoneNumber("9759064799").clickOnRegisterBtn();
+		
+		Log.info("Verify Registeration successfull message");
 		assertEquals(objAvactisHomePage.getAccountCreatedMessage(),
 				"Account created successfully. You are now registered.");
+		
+		Log.endTestCase("verifyRegisterationAffirmative");
 	}
 
 	@Test
 	public void verifyRegisterationNegative() {
+		Log.startTestCase("verifyRegisterationNegative");
+		
+		Log.info("Entering invalid user details for successfull registeration");
 		objRegisterPage = objAvactisHomePage.clickOnMyAccount().clickOnRegisterButton();
 		objRegisterPage.enterEmail("zia2208@gmail.com").enterPassword("9759064799").enterReTypePassword("9627841412")
 				.enterFirstName("Mumtaz").enterLastName("Husain").selectCountry("India").selectState("Gujarat")
 				.enterZipCode("380055").enterCity("Ahmedabad").enteraddressLine1(" c-101, Al arsh-3")
 				.enteraddressLine2("juhapura").enterPhoneNumber("9759064799").clickOnRegisterBtn();
+		
+		Log.info("Verify registeration failed message");
 		assertEquals(objRegisterPage.getIncorrectPasswordMessage(),
 				"The password you entered is incorrect. Please enter the correct password.");
+		
+		Log.endTestCase("verifyRegisterationNegative");
 	}
 }
